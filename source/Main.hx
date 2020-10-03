d192c1d106b7a8eba0f3db232b66493279d468c7
merrow zerow <merrow0@gmail.com>
2020-10-04 01:10:05 +0200

    Fix some more

--- source/Main.hx
+++ source/Main.hx
@@ -10,4 +10,14 @@ class Main extends Sprite
 		super();
 		addChild(new FlxGame(640, 480, PlayState, 1, 60, 60, true, true));
 	}
+
+	function requestFullScreen():Void
+	{
+		untyped js.Syntax.code("if (document.documentElement.requestFullscreen) document.documentElement.requestFullscreen(); else if (document.documentElement.mozRequestFullScreen) document.documentElement.mozRequestFullScreen(); else if (document.documentElement.webkitRequestFullscreen) document.documentElement.webkitRequestFullscreen(); else if (document.documentElement.msRequestFullscreen) document.documentElement.msRequestFullscreen();");
+	}
+
+	function cancelFullScreen():Void
+	{
+		untyped js.Syntax.code("if (document.exitFullscreen) document.exitFullscreen(); else if (document.mozCancelFullScreen) document.mozCancelFullScreen(); else if (document.webkitExitFullscreen) document.webkitExitFullscreen(); else if (document.msExitFullscreen) document.msExitFullscreen();");
+	}
 }
